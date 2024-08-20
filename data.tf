f0830e41e54d36e3520cb006de9c1f06660f65e0
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-08-20 15:41:01 -0700

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,35 @@
+data "aws_ssm_parameter" "parameter_list" {
+  name = "/image-pipeline/${var.project_name}/parameters"
+}
+
+data "aws_ssm_parameter" "secrets_list" {
+  count = local.has_secrets ? 1 : 0
+  name  = "/image-pipeline/${var.project_name}/secrets"
+}
+
+data "aws_ssm_parameter" "params" {
+  for_each = toset(local.parameter_list)
+  name     = "/image-pipeline/${var.project_name}/${each.value}"
+}
+
+data "aws_ssm_parameter" "ami" {
+  name = "/image-pipeline/${var.project_name}/ami"
+}
+
+data "aws_secretsmanager_secret" "secrets" {
+  for_each = local.nonsensitive_key_list
+  name     = "/image-pipeline/${var.project_name}/${each.key}"
+}
+
+data "aws_secretsmanager_secret_version" "secret_version" {
+  for_each = tomap({
+    for secret in local.nonsensitive_key_list :
+    secret => lookup(data.aws_secretsmanager_secret.secrets, secret).id
+  })
+  secret_id = each.value
+}
+
+# this will fail if the goss profile file doesnt exist
+data local_file goss_profile_exists {
+  filename =  "${var.goss_directory}/goss-files/${local.goss_profile}"
+}
