173730ffe6baff0ee7e17c6de010db3a683e50ac
Dave Arnold <dave@roknsound.com>
2024-08-22 11:38:50 -0700

    Refactor IAM role and instance profile creation code

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   linux_instance       = var.ssh_user == null ? false : true
   windows_instance     = var.windows_instance
-  iam_instance_profile = var.iam_instance_profile == null ? aws_iam_instance_profile.instance.name : var.iam_instance_profile
+  iam_instance_profile = var.iam_instance_profile == null ? one(aws_iam_instance_profile.instance).name : var.iam_instance_profile
   create_instance_role = var.iam_instance_profile == null ? true : false
 }
