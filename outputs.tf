db94a2cf92ea24efbb7a48799899139c42969343
Dave Arnold <dave@roknsound.com>
2024-08-22 12:13:26 -0700

    Refactor outputs.tf and templates/ssh files

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "linux_instnace" {
-  value = local.linux_instance ? aws_instance.linux : []
+
+output "linux_ssh" {
+  value = templatefile("$(path.module)/ssh", {
+    secret_id = aws_secretsmanager_secret.ssh_key[0].id,
+    key_name  = aws_key_pair.deployer.key_name,
+  })
 }
 
-# output "windows_instance" {
-#   value = local.windows_instance ? aws_instance.windows : []
-# }
+output "linux_hosts" {
+  value = local.linux_instance ? var.enable_public_ip ? aws_instance.linux[*].public_ip : aws_instance.linux[*].private_ip : []
+}
